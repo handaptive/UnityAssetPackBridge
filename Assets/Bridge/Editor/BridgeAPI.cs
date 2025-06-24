4db6e0fc0f2fe70cb1a427b6fea72d641801de55
Josiah Saunders <contact@josiahsaunders.com>
2025-06-24 08:54:37 -0600

    loopback server wip

---
+++ Assets/Bridge/Editor/BridgeAPI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace AssetPack.Bridge.Editor
+{
+  public static class BridgeAPI
+  {
+    [System.Serializable]
+    private class Tokens
+    {
+      public string idToken;
+      public string refreshToken;
+    }
+  }
+}
